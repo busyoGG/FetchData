758c7b0b903ddbae3f6518d53e6b164a65d22da5
朽朽朽朽朽 <694253220@qq.com>
2025-07-23 21:59:34 +0800

    修改提交的文件夹

--- src/update.sh
+++ src/update.sh
@@ -10,6 +10,6 @@ node src/hakush.js zzz && node src/fetchGachaDataV2.js zzz
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
-git add data/gacha/*
+git add data/*
 git commit -m "Daily gacha update: $(date -u '+%Y-%m-%d %H:%M:%S')" || echo "Nothing to commit"
 git push origin HEAD:${GITHUB_REF#refs/heads/}
